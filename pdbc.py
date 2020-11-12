4aa0a93076e4b2fa44890bfda7617477d7f33265
Prabhat Das <prabhatdash0376@gmail.com>
2020-11-12 10:51:32 +0530

    Update pdbc.py

--- pdbc.py
+++ pdbc.py
@@ -1,31 +1,32 @@
-import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="macintosh",database="covid")
-mycursor=mydb.cursor()
-print("USE THE FOLLOWING OPTIONS TO PROCEED")
-print("1 TO REGISTER( FOR NEW USERS) | 2 TO LOGIN")
-op=int(input())
-def inp():
-    print("iaminp")
-    email = input("Enter your mail_id to login")
-    print(email)
-    login_auth(email)
+#________________________________________________________________________
+import mysql.connector as mc
+dbc=mc.connect(host="localhost",user="root",passwd="macintosh",database="covid")
+cursor=dbc.cursor()
+#________________________________________________________________________
 
-def login_auth(email):
-    for i in mycursor:
+def user_auth():
+    email = input("Enter your email to login")
+    fetch_query = "select * from reg_users;"
+    cursor.execute(fetch_query)
+    for i in cursor:
         if i[1] == email:
             print("Login Successful !")
-        else:
-            print("Login Failed !")
-            inp()
-if op==1:
-    u_name=input("Enter your name")
-    email=input("Enter your mail_id")
-    sql="insert into reg_users(u_name,email) values (%s,%s)"
-    val=(u_name,email)
-    mycursor.execute(sql,val)
-    mydb.commit()
-    mycursor.execute("select * from reg_users")
-    inp()
-elif op==2:
-    mycursor.execute("select * from reg_users")
-    inp()
+            count = 1
+            break
+    if count == 0:
+        print("Login Error !")
+
+print("Enter 1 to Login or 2 to Register for new users")
+inp=int(input())
+count=0
+if inp==1:
+   user_auth()
+
+elif inp==2:
+    name = input("Enter your name")
+    email = input("Enter your email")
+    query = "insert into reg_users values('{}','{}');".format(name, email)
+    print(query)
+    cursor.execute(query)
+    dbc.commit()
+    user_auth()
